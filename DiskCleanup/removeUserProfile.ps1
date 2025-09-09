7f08f2ccd7189268b82f30e5b551710c545989e0
Cory Laidlaw <46942737+CoryLaidlaw@users.noreply.github.com>
2025-09-09 10:05:09 -0700

    Delete DiskCleanup/removeUserProfile.ps1

--- DiskCleanup/removeUserProfile.ps1
+++
@@ -1,7 +0,0 @@
-$UserProfile = "USERNAME"
-$ProfilePath = "C:\Users\USERNAME"
-
-# Remove user profile
-Get-WmiObject Win32_UserProfile | Where-Object { $_.LocalPath -eq $ProfilePath } | ForEach-Object { $_.Delete() }
-
-[math]::Round((Get-PSDrive C).Free / 1GB, 2)
